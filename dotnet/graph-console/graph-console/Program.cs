59ba03b0d2860c29b00b54fc534be8f0d0dd2bf9
Chris Joakim <christopher.joakim@gmail.com>
2019-02-03 15:58:40 -0500

    graph-console - 3rd json parsing impl

--- dotnet/graph-console/graph-console/Program.cs
+++ dotnet/graph-console/graph-console/Program.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Collections.Generic;
 using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
 
 // See https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/console-webapiclient
 
@@ -49,19 +50,34 @@ namespace graph_console {
                 new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
             httpClient.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
 
-            // Initial impl without JSON deserialization
-            var stringTask = httpClient.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
-            var msg = await stringTask;
-            Console.Write(msg);
+            int impl = 3;
 
-            // 2nd impl
-            var serializer = new DataContractJsonSerializer(typeof(List<Repo>));
-            var streamTask = httpClient.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
-            var repositories = serializer.ReadObject(await streamTask) as List<Repo>;
-            foreach (var repo in repositories) {
-                Console.WriteLine(repo.name);
+            if (impl == 1) {
+                // Initial impl without JSON deserialization
+                var stringTask = httpClient.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+                var msg = await stringTask;
+                Console.Write(msg);
             }
 
+            if (impl == 2) {
+                // 2nd impl
+                var serializer = new DataContractJsonSerializer(typeof(List<Repo>));
+                var streamTask = httpClient.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+                var repositories = serializer.ReadObject(await streamTask) as List<Repo>;
+                foreach (var repo in repositories) {
+                    Console.WriteLine(repo.name);
+                }
+            }
+
+            if (impl == 3) {
+                // 3rd impl
+                var serializer = new DataContractJsonSerializer(typeof(List<Repository>));
+                var streamTask = httpClient.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+                var repositories = serializer.ReadObject(await streamTask) as List<Repository>;
+                foreach (var repo in repositories) {
+                    Console.WriteLine(repo);
+                }
+            }
         }
 
         static void log(string msg) {
