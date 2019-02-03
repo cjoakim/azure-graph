069b4349fdd11f3ae351cac6295362f51a6e4b1e
Chris Joakim <christopher.joakim@gmail.com>
2019-02-03 12:40:19 -0500

    dotnet/graph-console/graph-console/Program.cs - reading access token file

--- dotnet/graph-console/graph-console/Program.cs
+++ dotnet/graph-console/graph-console/Program.cs
@@ -1,9 +1,43 @@
 ﻿using System;
+using System.IO;
 
 namespace graph_console {
+
     class Program {
+
+
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            log("Start of Main()");
+            string accessToken = readAccessToken();
+            log("accessToken: " + accessToken);
+
+            terminate();
+        }
+
+        static string readAccessToken() {
+
+            // [Environment]::SetEnvironmentVariable("GRAPH_CONSOLE_HOME", "C:\Users\chris\github\azure-graph\dotnet\graph-console\graph-console", "User")
+            string projectDir = Environment.GetEnvironmentVariable("GRAPH_CONSOLE_HOME");
+            string inputFile  = projectDir + "/tmp/access_token.txt";
+            log("inputFile: " + inputFile);
+
+            using (StreamReader sr = new StreamReader(inputFile)) {
+                return sr.ReadToEnd();
+            }
+        }
+
+        static void log(string msg) {
+
+            Console.WriteLine(msg);
+        }
+
+        static void terminate() {
+
+            log("Hit enter key to continue and terminate...");
+            string line = Console.ReadLine();
+            // log(line);
+            // System.Threading.Thread.Sleep(1000);
         }
     }
 }
