280ace00eb993f868b1480f97197ca0341aed650
Chris Joakim <christopher.joakim@gmail.com>
2019-02-03 15:29:04 -0500

    graph-console/Program.cs - added http client

--- dotnet/graph-console/graph-console/Program.cs
+++ dotnet/graph-console/graph-console/Program.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.IO;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+// See https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/console-webapiclient
 
 namespace graph_console {
 
     class Program {
 
+        private static readonly HttpClient httpClient = new HttpClient();
 
         static void Main(string[] args) {
 
             log("Start of Main()");
             string accessToken = readAccessToken();
             log("accessToken: " + accessToken);
+
+            getGithubDotnetRepositories().Wait();
 
             terminate();
         }
@@ -25,6 +33,19 @@ namespace graph_console {
             using (StreamReader sr = new StreamReader(inputFile)) {
                 return sr.ReadToEnd();
             }
+        }
+
+        private static async Task getGithubDotnetRepositories() {
+
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            httpClient.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var stringTask = httpClient.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+
+            var msg = await stringTask;
+            Console.Write(msg);
         }
 
         static void log(string msg) {
